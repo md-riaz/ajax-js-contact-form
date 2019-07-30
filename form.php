5cfc4676db6fccd0951fc10e0b2242593bc81d35
MD Riaz <riazmd582@gmail.com>
2019-07-30 19:32:46 +0600

    .

--- form.php
+++ form.php
@@ -24,13 +24,4 @@ mail($to,$email_subject,$email_body,$headers);
 echo 'Your mail has been sent successfully!';
 return true;		
 
-    // if( isset($_POST['some-name']) and !empty($_POST['some-name']) ){
-    // 	echo $_POST['some-name'];
-    // 	exit();
-    // }
-    // else{
-    // 	echo 'Please enter your name!';
-    // 	exit();
-    // }
-
 ?>
